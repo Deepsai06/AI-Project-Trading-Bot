07e7ef426d2d9f74eb1931c58e47198d46e63325
dna-2806 <divya.anand7740@gmail.com>
2025-04-21 01:18:11 +0530

    configuration files

---
+++ config.py
@@ -0,0 +1,7 @@
+import credentials
+ALPACA_CREDS = {
+    "API_KEY":credentials.api_key,
+    "API_SECRET":credentials.api_secret,
+    "PAPER":True
+}
+url = credentials.BASE_URL
---
+++ credentials.py
@@ -0,0 +1,3 @@
+api_key = "PKQ71SMUHVOK8NM8NWU0"
+api_secret = "7J4PGDhR8oXL0Abayj3CGbnGghZpsy4svd8xCraN"
+BASE_URL = "https://paper-api.alpaca.markets"
